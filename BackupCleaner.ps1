dceb7682a0732e97f298000eb0ec6d6a63bc165b
gsmyk <63458150+gsmyk@users.noreply.github.com>
2022-10-31 19:56:55 +0100

    Add files via upload
    DirectoryBackuper - make daily backup
    BackupCleaner - remove old backups

---
+++ BackupCleaner.ps1
@@ -0,0 +1,16 @@
+﻿$backupCleanerInstall = 'C:\scripts\BackupCleaner\'
+$backupCleanerScript = $backupCleanerInstall+"BackupCleaner.ps1"
+$backupCleanerLog = $backupCleanerInstall+"backupCleaner.log"
+
+$emptyDirectoryPath = '.\empty'
+$backupsPath = '.\BackupPath'
+
+$oldBackups = Get-ChildItem -Path $backupsPath -Directory | Where-Object { $_.CreationTime -lt (Get-Date).AddDays(-7) }
+
+foreach ($backup in $oldBackups) {
+    Write-Host $backup
+    $fullBackuPath = Join-Path -Path $backupsPath -ChildPath $backup
+    robocopy $emptyDirectoryPath $fullBackuPath /purge
+    Remove-Item $fullBackuPath
+    Write-Host "Backup removed: " $backup | Out-File $backupCleanerLog
+}
---
+++ DirectoryBackuper.ps1
@@ -0,0 +1,27 @@
+﻿$backuperInstall = 'C:\scripts\Backuper\'
+$backuperScript = $backuperInstall+"OneDriveBackuper.ps1"
+$backuperLog = $backuperInstall+"backuper_log.log"
+
+$sourcePath = 'Source-path'
+$destinationPath = 'Destination-Path'
+
+$today = Get-Date -UFormat "%Y-%m-%d"
+$currentBackup = 'GDDKiA CPW-'+$today
+
+$statTime = Get-Date -UFormat "%Y-%m-%d-%Hh%Mm%Ss"
+
+Write-Host "Start at: " $statTime
+Write-Host "From: " $sourcePath
+Write-Host "To: " $currentBackup
+
+"Backuper" | Out-File $backuperLog -append
+$statTime | Out-File $backuperLog -append
+$currentBackup | Out-File $backuperLog -append
+
+robocopy $sourcePath (Join-Path -Path $destinationPath -ChildPath $currentBackup) /e # | Out-File $backuperLog -append
+
+$finishTime = Get-Date -UFormat "%Y-%m-%d-%Hh%Mm%Ss"
+
+$finishTime | Out-File $backuperLog -append
+Write-Host "Finished at: " $finishTime
+"Backup is complete" | Out-File $backuperLog -append
